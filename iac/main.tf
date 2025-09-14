5a243e52d90562aed6cf8c377bc3aa19d670fff4
Bhargav Teja Guttula <143714131+bhargavteja1999@users.noreply.github.com>
2025-09-15 00:52:52 +0530

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -1,20 +1,11 @@
 provider "aws" {
-  region = "ap-south-1" # Change to your preferred AWS region
+  region = "us-east-1" # Change to your preferred AWS region
 }
 
 resource "aws_instance" "dev_ec2" {
-  ami           = "ami-0522ab6e1ddcc7055" # Ubuntu 22.04 LTS AMI for ap-south-1 (update if using a different region)
+  ami           = "ami-0360c520857e3138f" 
   instance_type = "t2.micro"
-  key_name      = "your-keypair-name" # Replace with your key pair name
-
-  user_data = <<-EOF
-    #!/bin/bash
-    apt-get update -y
-    apt-get install -y docker.io
-    systemctl start docker
-    systemctl enable docker
-    usermod -aG docker ubuntu
-  EOF
+  key_name      = "ansible-kp"
 
   tags = {
     Name = "Terraform-EC2-Ubuntu-Docker"
