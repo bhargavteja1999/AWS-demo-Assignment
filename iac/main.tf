d9aeeaa7031b943ae7f561b596b51f90d32d9241
Bhargav Teja Guttula <143714131+bhargavteja1999@users.noreply.github.com>
2025-09-15 12:34:13 +0530

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -1,15 +1,49 @@
 provider "aws" {
-  region = "us-east-1" # Change to your preferred AWS region
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "dev_sg" {
+  name        = "dev-ec2-sg"
+  description = "Allow all traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "dev-ec2-sg"
+  }
 }
 
 resource "aws_instance" "dev_ec2" {
-  ami           = "ami-0360c520857e3138f" 
+  ami           = "ami-0554aa6767e249943" # Amazon Linux 2 (us-east-1)
   instance_type = "t2.micro"
   key_name      = "ansible-kp"
+  vpc_security_group_ids = [aws_security_group.dev_sg.id]
 
   tags = {
-    Name = "Terraform-EC2-Ubuntu-Docker"
+    Name = "Terraform-EC2-AmazonLinux-Docker"
   }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              cd /home/ec2-user
+              git clone https://github.com/bhargavteja1999/AWS-demo-Assignment.git
+              cd AWS-demo-Assignment
+              chmod +x docker.sh
+              ./docker.sh
+              EOF
 }
 
 output "public_ip" {
