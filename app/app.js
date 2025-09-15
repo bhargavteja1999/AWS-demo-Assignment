c3e31baff87bb693cf86c52f6dfad7c20b384015
Bhargav Teja Guttula <143714131+bhargavteja1999@users.noreply.github.com>
2025-09-16 00:49:44 +0530

    Create app.js

---
+++ app/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+const PORT = process.env.PORT || 80;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Hello, from Node.js App!</h1>
+    <p>Served by: ${os.hostname()}</p>
+  `);
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK' });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on http://0.0.0.0:${PORT}`);
+});
