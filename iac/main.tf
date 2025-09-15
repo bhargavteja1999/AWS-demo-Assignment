9a34cb26896b9bcd23b9f6b7b29669898ed2152a
Bhargav Teja Guttula <143714131+bhargavteja1999@users.noreply.github.com>
2025-09-15 13:06:29 +0530

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "dev_sg" {
 }
 
 resource "aws_instance" "dev_ec2" {
-  ami           = "ami-0554aa6767e249943" # Amazon Linux 2 (us-east-1)
+  ami           = "ami-0554aa6767e249943" # Amazon Linux 2
   instance_type = "t2.micro"
   key_name      = "ansible-kp"
   vpc_security_group_ids = [aws_security_group.dev_sg.id]
@@ -38,6 +38,11 @@ resource "aws_instance" "dev_ec2" {
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
+              sudo yum install -y git docker
+              sudo systemctl enable docker
+              sudo systemctl start docker
+              sudo usermod -aG docker ec2-user
+
               cd /home/ec2-user
               git clone https://github.com/bhargavteja1999/AWS-demo-Assignment.git
               cd AWS-demo-Assignment
